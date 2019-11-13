fe49641f66c78d9a03d12a74f5bd5ec268e85574
Richard Paul <richysites@gmail.com>
2019-11-13 16:57:22 +1300

    shitty story writer working

--- Loony Lips/LoonyLips.gd
+++ Loony Lips/LoonyLips.gd
@@ -1,9 +1,40 @@
 extends Control
 
+var player_words = []
+var story = "%s had a %s and it was %s, he felt %s"
+var prompts = ["a name", "a noun", "an adjective", "another adjective"]
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/InputText
+onready var DisplayText = $VBoxContainer/DisplayText
+
 func _ready():
-	var prompts = ["Bob", "Bobs world", "shit"]
-	
-	var story = "%s watched %s and it was %s"
-	
-	#print(story % prompts)
-	$DisplayText.text = story % prompts
+	DisplayText.text = "Create a story!  "
+	check_player_words_length()
+
+
+func _on_Button_pressed():
+	add_to_player_words()
+
+func _on_InputText_text_entered(new_text):
+	add_to_player_words()
+
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + "."
